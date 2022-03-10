2c1503132fa463b252dc4bfa901dd4ea0c125b80
Prashanth TR <prashanth.thattai@gmail.com>
2022-03-10 12:43:07 +0000

    added encoder conversion

--- display_papers.py
+++ display_papers.py
@@ -8,7 +8,7 @@ with open("nime_abstracts.json") as json_file:
 with open('selected_papers.txt', 'w') as f:
     for paper in data:
         if len(paper["user_relevance"]) > 0 and sum(paper["user_relevance"]) >= 1:
-            f.write(str(paper["yearno"]) + " \n" + paper["authornames"] + " : " + paper["title"] + " : \n " + paper["abstract"] + " \n")
+            f.write(paper["yearno"].encode('ascii', 'ignore').decode('ascii') + " \n" + paper["authornames"] + " : " + paper["title"] + " : \n " + paper["abstract"] + " \n")
 
 print("\n")
 print("Please open the selected_papers.txt file available in the folder")
