c4bd61ec0cd30f5052a8857f1a9f833b94737630
Prashanth TR <prashanth.thattai@gmail.com>
2022-03-31 09:59:35 +0100

    first usable version of tagpaper

--- i-prompt.py
+++ i-prompt.py
@@ -5,6 +5,7 @@ import json
 import argparse
 import re
 import textwrap
+import os
 
 # from rake_nltk import Rake
 # r = Rake()
@@ -12,38 +13,39 @@ import textwrap
 def get_arguments():
 
     parser = argparse.ArgumentParser(description="myParser")
-    parser.add_argument("--keywords")
-    parser.add_argument("--abstracts")
+    parser.add_argument("--kw")
+    parser.add_argument("--db")
     return parser.parse_args()
 
 def main():
 
     #url, parser, extractor, thresohold: args 
     args = get_arguments()
-    prompt = args.keywords
-    abstracts = args.abstracts
+    prompt = args.kw
+    abstracts = args.db
     get_papers(prompt, abstracts)
 
 def get_papers(keywords, abstracts):
+    
     # from json dictionary
 
     # r.extract_keywords_from_text(prompt)
     # prompt_keywords = r.get_ranked_phrases()
 
     # Load dictionary (current json file)
-    with open(abstracts) as json_file:
+    with open("db/"+abstracts) as json_file:
         data = json.load(json_file)
 
-    kw_file = open(keywords, "r") 
+    kw_file = open("keywords/" + keywords, "r") 
     prompt_keywords = kw_file.read().split("\n")
     
     # Remove whitespaces
     for pk in prompt_keywords:
         pk = pk.strip();
 
-    #prompt_keywords = ["embodied", "embodied experience", "embodiment", "music cognition", "evaluation", "experience", "ontology", "presence", "new music experiences", "accessibility", "inclusivity", "flow", "knowledge", "perception", "digital score", "notation", "musicainship", "emergence", "real-time score"]
-    #prompt_keywords = ["liveness", "EEG", "instrument", "skills", "awareness", "context", "musicianship"]
-    #print(prompt_keywords)
+    # prompt_keywords = ["embodied", "embodied experience", "embodiment", "music cognition", "evaluation", "experience", "ontology", "presence", "new music experiences", "accessibility", "inclusivity", "flow", "knowledge", "perception", "digital score", "notation", "musicainship", "emergence", "real-time score"]
+    # prompt_keywords = ["liveness", "EEG", "instrument", "skills", "awareness", "context", "musicianship"]
+    # print(prompt_keywords)
 
     match_results = []
 
@@ -80,29 +82,39 @@ def get_papers(keywords, abstracts):
         print(textwrap.fill(paper["abstract"], 100))
         print(" \n ")
         print("Please label if this is relevant, 1 for yes, 0 for no")
-        relevant = int(input())
+        relevant = input()
 
-        while relevant > 1 :
-            print("Press value between 0 and 1")
-            relevant = int(input())
+        while not relevant.isnumeric() or int(relevant) > 1 :
+            print("Press value between 0 or 1")
+            relevant = input()
+
+        if int(relevant) == 0 or int(relevant) == 1:
+           relevant = int(relevant) 
 
         paper["user_relevance"].append(relevant)#(1/0)
         paper["prompt_keywords"].append(prompt_keywords) # HERE also, create a unique list of common keywords
 
-    # USER LABELS THE RELEVANCE OF PAPERS SELECTED FROM THE CLASSIFICATION ALGORITHM
-    # SOLOMIYA: ROUGHLY HOW MANY WORDS MATCH
-    # 
-
-    for data_item in data:
-        
-        for paper in match_results:
+        # Writes a paper to database everytime solomiya selects it as relevant
+        for data_item in data:
             if data_item["title"] == paper["title"]:
                 data_item = paper
                 break
 
-    # rewrite new file
-    with open('nime_abstracts.json', 'w') as outfile:
-        json.dump(data, outfile, indent=4, sort_keys=True)
+        with open("db/"+abstracts, 'w') as outfile:
+            json.dump(data, outfile, indent=4, sort_keys=True)
+
+        with open("selected_papers/" + os.path.splitext(abstracts)[0] + "-" + os.path.splitext(keywords)[0] + ".txt", 'w') as f:
+            for paper in data:
+                if len(paper["user_relevance"]) > 0 and sum(paper["user_relevance"]) >= 1:
+                    f.write(paper["yearno"].encode('ascii', 'ignore').decode('ascii') + " \n" + paper["authornames"] + " : " + paper["title"] + " : \n " + paper["abstract"] + " \n")
+
+        print("\n")
+        print("Please open the selected_papers.txt file available in the folder")
+        print("\n")
+
+    # USER LABELS THE RELEVANCE OF PAPERS SELECTED FROM THE CLASSIFICATION ALGORITHM
+    # SOLOMIYA: ROUGHLY HOW MANY WORDS MATCH
+    # 
 
 if __name__ == '__main__':
     main()
